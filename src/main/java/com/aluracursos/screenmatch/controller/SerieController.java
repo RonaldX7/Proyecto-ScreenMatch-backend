4cd56b287e5d14538b5705fb75385281e85abf91
RonaldX7 <ronaldjair0705@gmail.com>
2024-06-29 18:15:19 -0500

    cambios añadidos

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -6,25 +6,38 @@ import com.aluracursos.screenmatch.repository.SerieRepository;
 import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService servicio;
 
-    @GetMapping("/series")
+    @GetMapping()
     public List<SerieDTO> obtenerLasSeries(){
         return servicio.obtenerLasSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obtenerTop5Series() {
         return servicio.obtenerTop5();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerLanzamientosMasRecientes() {
+        return servicio.obtenerLanzamientosMasRecientes();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtenerSeriePorId(@PathVariable Long id) {
+        return servicio.obtenerPorId(id);
     }
 }
 
