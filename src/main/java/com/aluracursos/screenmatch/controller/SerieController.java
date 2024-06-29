9bcde083e01d9cf42cc646c87f019f8fc89a0bd0
RonaldX7 <ronaldjair0705@gmail.com>
2024-06-29 17:26:23 -0500

    nuevos cambios

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.controller;
 import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
+import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +15,16 @@ import java.util.stream.Collectors;
 public class SerieController {
 
     @Autowired
-    private SerieRepository repository;
+    private SerieService servicio;
 
     @GetMapping("/series")
     public List<SerieDTO> obtenerLasSeries(){
-        return repository.findAll().stream()
-                .map(s -> new SerieDTO(s.getTitulo(),s.getTotalTemporadas(),s.getEvaluacion(),
-                        s.getPoster(),s.getGenero(),s.getActores(),s.getSinopsis()))
-                .collect(Collectors.toList());
+        return servicio.obtenerLasSeries();
+    }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> obtenerTop5Series() {
+        return servicio.obtenerTop5();
     }
 }
 
